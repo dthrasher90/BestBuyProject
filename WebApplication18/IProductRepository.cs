a21e28e3de5d985b09740c28303be70172fa6383
dthrasher90 <andrew@mypetsemergencyid.com>
2021-08-30 11:42:24 -0700

    initial working
    main working

---
+++ WebApplication18/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication18.Models;
+
+namespace WebApplication18
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+
+        public void DeleteProduct(Product product);
+
+
+    }
+}
---
+++ WebApplication18/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication18.Models;
+
+namespace WebApplication18.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
